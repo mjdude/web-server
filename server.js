dd0a2d63d1e9ce2f8d7cee00cb38679cbaa769b0
mjdude <mo@syncspot.net>
2016-04-06 17:17:08 +0100

    initialise repo

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var middleware = require('./middleware.js');
+var app = express();
+var PORT = 3000;
+
+app.use(middleware.logger);
+app.get('/about',middleware.requireAuthentication ,function(req, res){
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+  console.log('express server started on port ' + PORT);
+
+});
